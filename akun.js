6bdf1f13aa775c6d3f9c3ffbfc9985ed6584b6a7
Mutia09 <cutmutia3564@gmail.com>
2026-05-01 23:56:19 +0700

    Menambahkan index.html dan akun.js

--- akun.js
+++ akun.js
@@ -1,1 +1,55 @@
+// Ambil data user dari localStorage
+function getUsers() {
+    return JSON.parse(localStorage.getItem("users")) || [];
+}
 
+// Simpan user ke localStorage
+function saveUsers(users) {
+    localStorage.setItem("users", JSON.stringify(users));
+}
+
+// Register
+function register(username, password) {
+    let users = getUsers();
+
+    let userExists = users.find(user => user.username === username);
+    if (userExists) {
+        alert("Username sudah digunakan!");
+        return false;
+    }
+
+    // simpan user baru
+    users.push({ username, password });
+    saveUsers(users);
+
+    alert("Berhasil daftar!");
+    return true;
+}
+
+// Login
+function login(username, password) {
+    let users = getUsers();
+
+    let user = users.find(
+        user => user.username === username && user.password === password
+    );
+
+    if (user) {
+        localStorage.setItem("loggedInUser", username);
+        alert("Login berhasil!");
+        return true;
+    } else {
+        alert("Username atau password salah!");
+        return false;
+    }
+}
+
+// Logout
+function logout() {
+    localStorage.removeItem("loggedInUser");
+    alert("Logout berhasil!");
+}
+
+function isLoggedIn() {
+    return localStorage.getItem("loggedInUser") !== null;
+}
